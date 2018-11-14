e8f41e00e17fa566858e7da062606461490bf6b3
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-14 05:12:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,6 @@
 def speak_to_grandma(phrase)
     if phrase != phrase.upcase()
       return "HUH?! SPEAK UP, SONNY!"
-  end	    else
+    else
        return "NO, NOT SINCE 1938!"
-  it 'responds with HUH?! SPEAK UP, S
+end
